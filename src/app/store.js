8e8aecd1a987a8e6b4b09f0244d6857ca9c0978c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-05-06 14:10:01 -0400

    starter

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
 });
+
+// run -  npx create-react-app cra-redux --template redux   - to create react app with redux, where you have  redux-toolkit already set-up for you.
+//This configureStore() has redux-devtool and redux-thunk installed by defaul. So we don't have to configure them in here.
