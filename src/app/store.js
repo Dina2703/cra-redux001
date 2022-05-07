409c06d21c8cb76e75b61d2d253c2966de8754ce
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-05-06 20:37:10 -0400

    cleanup before the Doto app

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
 
 // run -  npx create-react-app cra-redux --template redux   - to create react app with redux, where you have  redux-toolkit already set-up for you.
